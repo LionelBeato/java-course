db9e10fa1f873a42f35c0f42db185d1dc6e92079
Lionel Beato <lionel.beato@gmail.com>
2022-04-22 17:07:40 -0400

    add session-2

---
+++ session-2/App.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ * topics:
+ * - functions
+ * - data types
+ * - objects
+ * 
+ */
+
+public class App {
+
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal(); 
+        myAnimal.speak(); 
+    }
+
+}
+
+/**
+ * 
+ *  https://www.baeldung.com/java-run-jar-with-arguments
+ * 
+ */
