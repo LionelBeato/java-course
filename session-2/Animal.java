cd07664d5d5d6fd477f4ddc00e770fb4029a98d0
Lionel Beato <lionel.beato@gmail.com>
2022-04-29 14:12:03 -0400

    add comments and more example code

--- session-2/Animal.java
+++ session-2/Animal.java
@@ -1,6 +1,36 @@
+/**
+ * 
+ * In Java, everything is a class. 
+ * Classes serve as wrappers for your code
+ * but in a true object-oriented sense,
+ * they act as "blueprints" for objects
+ * 
+ */
+
 public class Animal {
 
+    /**
+     * 
+     * Classes can have properties
+     * known as fields. 
+     * These are in essence the class' own personal variables 
+     * 
+     */
+    String name; 
+    String color;
+
+
+    /**
+     * 
+     * whenever a function belongs to class
+     * its known as a method 
+     * 
+     * Since all functions will belong to classes in Java
+     * the vast majority of them are referred to as methods 
+     * 
+     *  */ 
     public void speak() {
         System.out.println("This animal spoke!");
     }
+    
 }
--- session-2/App.java
+++ session-2/App.java
@@ -1,6 +1,7 @@
 /**
  * 
  * topics:
+ * - standard features
  * - functions
  * - data types
  * - objects
@@ -10,7 +11,66 @@
 
 public class App {
 
+
+    /**
+     * 
+     * Below is a function
+     * functions allow you to encapsulate
+     * and reuse blocks of code  
+     * 
+     */
+    public static void printHello() {
+        System.out.println("Hello from a function!");
+    }
+
+
+
     public static void main(String[] args) {
+
+        /**
+         * 
+         * Java supports standard programming features such as
+         * arithmetic, flow control, and error handling 
+         * 
+         * While these are important, 
+         * they wont be the focus of this course 
+         * 
+         */
+
+
+        /**
+         * 
+         * An array, or collection, of numbers
+         * 
+         */
+        int[] numArr = {1,2,3,4,5}; 
+
+        /**
+         * 
+         * Standard for loop
+         * read it like so:
+         * "for every number in my number Array...
+         * ...print out that number's value"
+         * 
+         * 
+         */
+        for (int num : numArr) {
+            System.out.println(num);
+        }
+
+
+        /**
+         * 
+         * Below we are _instatiating_ an Animal, 
+         * thus creating an Animal _instance_
+         * 
+         * notice how we used "Animal" in place of "var"
+         * this is its _type_
+         * 
+         * there are many different types in the Java ecosystem,
+         * in truth countless. The reason? Every class defines a type. 
+         * 
+         */
         Animal myAnimal = new Animal(); 
         myAnimal.speak(); 
     }
